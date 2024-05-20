639e26b26f718a09ed403b7b8d389ebb460aeb8a
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2024-05-20 16:27:40 +0300

    release: v0.6.0

--- index.ts
+++ index.ts
@@ -10,85 +10,94 @@
 import bunrePckg from './package.json' assert { type: 'json' };
 import { join } from 'node:path';
 import fs from 'node:fs';
+import assert from 'node:assert';
+import { $ } from 'bun'
 
-// function log(...args: any[]) {
-//     console.log(`\x1b[34m[${bunrePckg.name}]\x1b[0m`, ...args);
-// }
-
-const log = console.log
-const $log = async (...args: any[]) => {
-    args = await Promise.all(args.map(async (arg) => await arg.text()));
-    args = args.map((arg) => arg.trim());
-    log(...args);
+async function _(bun$Output: any) {
+    return (await bun$Output.text()).trim()
 }
 
-const cwd = process.cwd();
-const pkgPath = join(cwd, 'package.json');
-const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-const version = pkg.version.split('.').map((v: string) => parseInt(v));
-const newVersion = [...version];
-
-log(bunrePckg.name, bunrePckg.version);
-log(pkg.name, pkg.version);
-
-const gitStatusOutput = await Bun.$`git status --porcelain`.text();
-const gitLogOutput = await Bun.$`git log origin..HEAD --oneline`.text();
-
-if (gitLogOutput.trim() === "") {
-    log("git log is empty.");
-} else {
-    if (gitLogOutput.includes('breaking:')) {
-        newVersion[0]++;
-        newVersion[1] = 0;
-        newVersion[2] = 0;
-    } else if (gitLogOutput.includes('feat:')) {
-        newVersion[1]++;
-        newVersion[2] = 0;
-    } else if (gitLogOutput.includes('fix:') || gitLogOutput.includes('docs:') || gitLogOutput.includes('test:')) {
-        newVersion[2]++;
-    }
-    log('git log origin..HEAD --oneline', gitLogOutput)
-    log('new version', newVersion)
+function log(...args: any[]) {
+    console.log(`\x1b[34m[${bunrePckg.name}]\x1b[0m`, ...args);
 }
 
-const newVersionStr = newVersion.join('.');
-const versionIsSignificant = newVersionStr !== pkg.version;
-log('new versionStr', newVersionStr, versionIsSignificant)
-if (versionIsSignificant) {
-    // git add tag
-    log("comminting version", newVersionStr)
-    // git delete tag
-    // git delete tag from origin
-    try {
-        await Bun.$`git tag -d v${newVersionStr}`.text();
-        await Bun.$`git push --delete origin v${newVersionStr}`.text();
-    } catch (e) {
-        log('no tag to delete')
-    }
-    //log('git tag -d v${newVersionStr}', gitPushTagsOutput)
-    await Bun.$`git tag -a v${newVersionStr} -m "release: v${newVersionStr}"`.text();
-    //git push
-    const gitPushOriginOutput = await Bun.$`git push origin v${newVersionStr}`.text();
-    log('git push origin', gitPushOriginOutput)
-    // git push tags
-    const gitPushTagsOutput = await Bun.$`git push origin --tags`.text();
-    log('git push origin --tags', gitPushTagsOutput)
+function info(...args: any[]) {
+    args = args.map((arg) => `\x1b[2m${arg}\x1b[0m`)
+    console.log(`\x1b[2m[${bunrePckg.name}]\x1b[0m`, ...args);
+}
 
-} else if (gitStatusOutput.trim() !== "") {
-    log("git status is not clean. commiting chore: progress");
-    log('git status', gitStatusOutput)
-    const gitAddallOutput = await Bun.$`git add .`.text();
-    const gitCommitChoreOutput = await Bun.$`git commit -m "chore: progress"`.text();
+const cwd = process.cwd()
+const pckg = JSON.parse(fs.readFileSync(join(cwd, 'package.json'), 'utf8'));
+const pckgVer = pckg.version
 
-    log('git add .', gitAddallOutput)
-    log('git commit -m "chore: progress"', gitCommitChoreOutput)
+assert(pckgVer, 'package.json version is not defined')
 
-    try {
-        const gitPushChoreOutput = await Bun.$`git push origin`.text();
-        log('git push', gitPushChoreOutput)
-        log('pushed chore: progress')
-    } catch (e) {
-        log('please check your ~/.ssh/config and update origin in ./.git/config or check keys settings if you are using tools like gitkraken')
-        log('git push', e)
-    }
-} 
+// major.minor.patch
+const pckgVerParts = pckgVer.split('.')
+
+info('bunre version', bunrePckg.version)
+log('cwd', cwd)
+log('current package name', pckg.name)
+log('current package version', pckgVer)
+
+const gitFetchOriginOutput = await _($`git fetch origin`)
+log('git fetch origin', gitFetchOriginOutput)
+
+const gitStatusOutput = await _($`git status --short`)
+log('git status', gitStatusOutput)
+
+const gitLogOnelineOutput = await _($`git log origin..HEAD --oneline`)
+log('git log origin..HEAD', '\n', gitLogOnelineOutput)
+
+if (gitLogOnelineOutput.includes('breaking:')) {
+    pckgVerParts[0]++
+    pckgVerParts[1] = 0
+    pckgVerParts[2] = 0
+} else if (gitLogOnelineOutput.includes('feat:')) {
+    pckgVerParts[1]++
+    pckgVerParts[2] = 0
+} else if (gitLogOnelineOutput.includes('fix:')) {
+    pckgVerParts[2]++
+} else if (gitLogOnelineOutput.includes('chore:')) {
+    pckgVerParts[2]++
+} else if (gitLogOnelineOutput.includes('docs:')) {
+    pckgVerParts[2]++
+}
+
+const newVersionName = pckgVerParts.join('.')
+log('new version name', newVersionName)
+
+const newTagName = `v${newVersionName}`
+log('new tag name', newTagName)
+
+if (newVersionName !== pckgVer) {
+    pckg.version = newVersionName
+
+    log('updated package version to', pckg.version)
+    fs.writeFileSync(join(cwd, 'package.json'), JSON.stringify(pckg, null, 2))
+
+    const gitAddPckgOutput = await _($`git add package.json`)
+    log('git add package.json', gitAddPckgOutput)
+
+    const gitCommitChoreOutput = await _($`git commit -m "release: ${newTagName}"`)
+    log('git commit -m "release: ${newTagName}"', gitCommitChoreOutput)
+}
+
+// process.exit(0)
+
+try {
+    const gitTagShowOutput = await _($`git show ${newTagName}`)
+    const gitRemoveLocalTagOutput = await _($`git tag -d ${newTagName}`)
+    const gitRemoveTagFromOriginOutput = await _($`git push origin --delete ${newTagName}`)
+} catch (e: any) {
+    log(`no tag named ${newTagName}`)
+}
+
+const gitTagAddOutput = await _($`git tag -a ${newTagName} -m "release: ${newTagName}"`)
+log('git tag -a', gitTagAddOutput)
+
+const gitTagPushOutput = await _($`git push origin`)
+log('git push origin', gitTagPushOutput)
+
+const gitPushTagsOutput = await _($`git push origin --tags`)
+log('git push origin --tags', gitPushTagsOutput)
