ba085ecf09af960fde13490c8b13ee0e66a54f27
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2024-05-30 06:48:59 +0300

    feat: Changelog

--- changelog.ts
+++ changelog.ts
@@ -1,17 +1,60 @@
-abstract class GitWrapper {
-    // async method to fetch git status in a machine readable format
-    static async status() {
-        return Bun.$`git status --short`.text()
+import fs from 'node:fs'
+import { join } from 'node:path'
+
+abstract class Bunre {
+    static _packageFilePath = join(process.cwd(), 'package.json')
+    static _package = JSON.parse(
+        fs.readFileSync(Bunre._packageFilePath, 'utf-8')
+    )
+
+    static git = {
+        async status() {
+            return await Bun.$`git status --short`.text()
+        },
+
+        async log() {
+            return await Bun.$`git log origin..HEAD --oneline`.text()
+        }
     }
 
-    // async method to show git log in a machine readable format
-    static async log() {
-        return Bun.$`git log origin..HEAD --oneline`.text()
+    static package = {
+        version: {
+            current: Bunre._package.version,
+
+            parse() {
+                return Bunre._package.version.split('.').map(Number)
+            },
+
+            bump(hasPatch = false, hasMinor = false, hasMajor = false) {
+                const [major, minor, patch] = Bunre.package.version.parse()
+
+                // if (hasMajor) {
+                //     return `${Number(major) + 1}.0.0`
+                // } else if (hasMinor) {
+                //     return `${major}.${Number(minor) + 1}.0`
+                // } else if (hasPatch) {
+                //     return `${major}.${minor}.${Number(patch) + 1}`
+                // }
+                if (hasMajor) {
+                    return `${major + 1}.0.0`
+                } else if (hasMinor) {
+                    return `${major}.${minor + 1}.0`
+                } else if (hasPatch) {
+                    return `${major}.${minor}.${patch + 1}`
+                } else {
+                    return Bunre.package.version.current
+                }
+            }
+        }
     }
 }
 
-const gitStatusOutput = await GitWrapper.status()
-const gitLogOnelineOutput = await GitWrapper.log()
+const gitStatusOutput = await Bunre.git.status()
+const gitLogOnelineOutput = await Bunre.git.log()
+const commitPrefixes = {
+    major: ['BREAKING CHANGE', 'major'],
+    minor: ['feat', 'minor'],
+    patch: ['fix', 'patch', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']
+}
 
-console.log(gitStatusOutput)
 console.log(gitLogOnelineOutput)
